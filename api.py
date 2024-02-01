d3517de0c24f533f307c28ae461d7355db4739b3
VIRUSGAMING64 <virusroger6@gmail.com>
2024-01-31 21:30:07 -0500

    2.0.0.1

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ def mainapi():
     
     print("runing api")
     if 1:
-        host = "0.0.0.0"
+        host = "http://0.0.0.0"
     else:
         host = "127.0.0.1"
-    uvicorn.run(app,host=host,port = 80,ws_max_queue=2**8-1,ws_max_size=2**16-1)
+    uvicorn.run(app,host=host,port = 8181,ws_max_queue=2**8-1,ws_max_size=2**16-1)
