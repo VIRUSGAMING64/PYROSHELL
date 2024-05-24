571c01985e7681c80a435168b12f32c8867b60bc
VIRUSGAMING64 <virusroger6@gmail.com>
2024-05-23 23:29:16 -0400

    3.2.0.4

--- modules/Utils.py
+++ modules/Utils.py
@@ -538,8 +538,11 @@ def getZ(msg):
 
 
 def vid_down(usr,msg:Message,bot:pyrogram.client.Client):
-    MyDownloader(bot,usr).download_video(msg.text)
-    
+    try:
+        MyDownloader(bot,usr).download_video(msg.text)
+    except Exception as e:
+        Gvar.LOG.append(str(e))
+        print(e)
 def USER_PROCCESS(USER, message: Message,bot:pyrogram.client.Client):
     MSG = str(message.text)
     if MSG.startswith("http") or MSG.startswith("https"):
